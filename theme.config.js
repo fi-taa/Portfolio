225f971c4b16c914d926f2654550fd7382fc9e2d
Fita Wegene <fi27ta@gmial.com>
2024-06-26 11:49:39 +0300

    intial commit

---
+++ theme.config.js
@@ -0,0 +1,15 @@
+// theme.config.js
+export default {
+    logo: <span>Fi Portfolio</span>,
+    nav: [
+      { text: 'Home', href: '/' },
+      { text: 'About', href: '/about' },
+      { text: 'Projects', href: '/projects' },
+      { text: 'Flutter', href: '/flutter' },
+      { text: 'Integration', href: '/integration' },
+      { text: 'ML', href: '/ml' },
+      { text: 'UI/UX', href: '/ux' },
+    ],
+    // Additional customization options
+  }
+  
