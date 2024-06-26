96ec38207535ca742f3bfde5a7e079bc2998ee7a
Fita Wegene <fi27ta@gmial.com>
2024-06-26 14:53:50 +0300

    added flutter projects

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,10 +4,10 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Fita Wegene</span>,
   project: {
-    link: 'https://github.com/fi-taa/Portfolio',
+    link: 'https://github.com/fi-taa',
   },
 
-  docsRepositoryBase: 'https://github.com/fi-taa/Portfolio',
+  docsRepositoryBase: 'https://github.com/fi-taa/portfolio',
   footer: {
     text: 'MIT 2023 © Fita Wegene',
   },
